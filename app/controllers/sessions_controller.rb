f431c0b4f9fc6d5a88010d5f759a4eb34f120b5c
Ian Carlos <carlos.ian007@gmail.com>
2019-04-11 13:14:50 +0000

    Implementing Basic Login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # redirect to user page
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password'
       render 'new'
@@ -14,6 +15,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    log_out
+    redirect_to root_url
   end
 
 end
